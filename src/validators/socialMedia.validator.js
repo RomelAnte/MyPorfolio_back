2d4f9d7d8e89aee3f4a6134703d61683ea92ddfc
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2026-01-13 21:20:46 -0500

    clean repo

--- src/validators/socialMedia.validator.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const socialMediaSchema = Joi.object({
-    platform: Joi.string().min(3).max(50).required(),
-    url: Joi.string().uri().max(255).required(),
-});
-
-module.exports = socialMediaSchema;
