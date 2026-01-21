8ab1bef877200589a521a9b37c6b1910d41e9515
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2026-01-20 20:21:35 -0500

    feat(migrations): create profile table with necessary fields

--- migrations/20260114034841_create_table_profile.js
+++ migrations/20260114034841_create_table_profile.js
@@ -3,7 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('profile', function(table) {
+        table.increments('id').primary();
+        table.string('full_name').notNullable();
+        table.text('bio');
+        table.string('photo_url');
+        table.integer('experience_years');
+        table.timestamps(true, true);
+    });
 };
 
 /**
@@ -11,5 +18,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('profile');
 };
