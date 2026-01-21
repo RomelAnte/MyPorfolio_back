05de495574e6b0436b0855ebbb11cee57640eefe
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2026-01-20 21:45:37 -0500

    feat(migrations): add project_technologies and experiences tables with necessary fields

---
+++ migrations/20260121024255_create_table_project_technologies.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('project_technologies', function(table) {
+        table.increments('id').primary();
+        table.integer('project_id').unsigned().references('id').inTable('projects').onDelete('CASCADE');
+        table.string('name').notNullable();
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('project_technologies');
+};
