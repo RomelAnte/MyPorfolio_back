c4ba085d79a42773a66ddb22bec30896b5178b53
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2026-01-20 22:03:09 -0500

    feat(migrations): create experiences table with necessary fields

--- migrations/20260121024531_create_table_experiences.js
+++ migrations/20260121024531_create_table_experiences.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('experiences', function(table) {
+        table.increments('id').primary();
+        table.string('role').notNullable();
+        table.string('company').notNullable();
+        table.date('start_date').notNullable();
+        table.date('end_date');
+        table.text('description');
+        table.boolean('is_current').defaultTo(false);
+        table.timestamps(true, true);
+    });
 };
 
 /**
@@ -11,5 +20,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('experiences');
 };
