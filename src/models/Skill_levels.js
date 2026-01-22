7e34e216e3b41d6b49c105150285d85ab2f65368
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2026-01-21 19:15:14 -0500

    feat(models): add relation mappings for Skill_levels model and create Projects model file

--- src/models/Skill_levels.js
+++ src/models/Skill_levels.js
@@ -1,8 +1,22 @@
 const { Model } = require('objection');
+const Skills = require('./Skills');
 
 class Skill_levels extends Model {
   static get tableName() {
     return 'skill_levels';
+  }
+
+  static get relationMappings() {
+    return {
+      skill: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Skills,
+        join: {
+            from: 'skill_levels.skill_id',
+            to: 'skills.id',    
+        },
+      },
+    };
   }
 }
 
